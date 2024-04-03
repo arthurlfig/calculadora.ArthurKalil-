f6f7850cece0fd8262f093ae2c624362276cf69f
arthurlfig <arthurklfig@gmail.com>
2024-04-03 20:23:48 -0300

    coloquei o while na calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,22 @@
-#calculadora
-A = float(input("digite o primeiro numero: "))
-B = float(input("digite o segundo numero: "))
-operação = input("digite o tipo de operação sendo 1- soma 2-subtração 3-multiplicação 4-divisão 5-sair : ")
+op = 0 
+while op != 5: 
+    print("\n escolha sua operação \n 1-soma \n 2- subtração \n 3-multiplicação \n 4-divisão \n 5- sair ")
+    op =int(input('\n digite sua escolha d eoperação: '))
 
-if operação == "1":
-     print(A + B)
-elif operação == "2":
-   print(A - B)
-elif operação == "3":
-     print(A * B)
-elif operação == "4":
-    print(A / B)
+    if op > 5 or op < 0:
+          print("digite sua opção de 1 a 5: ")
+          continue 
+    elif op == 5:
+         print("saiu da calculadora")
+         break
+    n1 = int(input('digite o primeiro numero que vai fazer parte da operação: \n'))
+    n2 = int(input('digite o segundo numero que vai fazer parte da operação: \n'))
+
+    if op == 1:
+         print(f"a soma de {n1} + {n2} é de \n {n1 + n2}")
+    elif op == 2:
+         print(f'a subtração de {n1} - {n2} é de \n {n1 - n2}')
+    elif op == 3:
+         print(f'a subtração de {n1} x {n2} é de \n {n1 * n2}')
+    elif op == 4:
+         print(f'a subtração de {n1} / \n {n2} é de \n {n1  / n2}')
